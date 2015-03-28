83acb5bc85f12126006be9d8a65a1ea243af1cb2
Justin Maat <justin.maat@wendys.com>
2015-03-28 14:41:16 -0400

    adding secrets file back

---
+++ config/secrets.js
@@ -0,0 +1,14 @@
+/**
+ * DANGER!! DANGER!!  DANGER!! DANGER!!  DANGER!! DANGER!!
+ *
+ * Never commit any real data to this file. Real keys/passwords
+ * should never be on github or in source control!!
+ *
+ * DANGER!! DANGER!!  DANGER!! DANGER!!  DANGER!! DANGER!!
+ */
+
+module.exports = {
+
+    api_key: 'some_hashed_key',
+    api_secret: 'some_hashed_secret'
+};
