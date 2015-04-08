0dfb36cd2c6b0c0db60333b54413f3e65421c5f4
Justin Maat <justin.maat@wendys.com>
2015-04-07 23:01:28 -0400

    adding tests for order and history transactions

--- test/bitfinex-node.test.js
+++ test/bitfinex-node.test.js
@@ -191,6 +191,130 @@ describe('bitfinex', function () {
             });
         });
 
+        describe('replaceOrder function', function () {
+            it('returns promise called with post(/order/cancel/replace) with passed in valid params', function () {
+                var postSpy = sandbox.spy(request, 'post');
+                var sendSpy = sandbox.spy(request.Request.prototype, 'send');
+                var params = {
+                    order_id: 1,
+                    symbol: 'btcusd',
+                    amount: 1,
+                    price: 1,
+                    exchange: 'bitfinex',
+                    side: 'buy',
+                    type: 'market',
+                    is_hidden: true
+                };
+
+                bitfinex.replaceOrder(params).should.have.property('then');
+                postSpy.should.have.been.calledWith('https://api.bitfinex.com/v1/order/cancel/replace');
+                sendSpy.should.have.been.calledWith(params);
+            });
+        });
+
+        describe('orderStatus function', function () {
+            it('returns promise called with post(/order/status) with passed in valid params', function () {
+                var postSpy = sandbox.spy(request, 'post');
+                var sendSpy = sandbox.spy(request.Request.prototype, 'send');
+                var params = {order_id: 1};
+
+                bitfinex.orderStatus(params).should.have.property('then');
+                postSpy.should.have.been.calledWith('https://api.bitfinex.com/v1/order/status');
+                sendSpy.should.have.been.calledWith(params);
+            });
+        });
+
+        describe('positions function', function () {
+            it('returns promise called with post(/order/status) with passed in valid params', function () {
+                var postSpy = sandbox.spy(request, 'post');
+                var sendSpy = sandbox.spy(request.Request.prototype, 'send');
+
+                bitfinex.positions().should.have.property('then');
+                postSpy.should.have.been.calledWith('https://api.bitfinex.com/v1/positions');
+                sendSpy.should.have.been.called;
+            });
+        });
+
+        describe('claimPosition function', function () {
+            it('returns promise called with post(/position/claim) with passed in valid params', function () {
+                var postSpy = sandbox.spy(request, 'post');
+                var sendSpy = sandbox.spy(request.Request.prototype, 'send');
+                var params = {order_id: 1};
+
+                bitfinex.claimPosition(params).should.have.property('then');
+                postSpy.should.have.been.calledWith('https://api.bitfinex.com/v1/position/claim');
+                sendSpy.should.have.been.calledWith(params);
+            });
+        });
+
+        describe('balanceHistory function', function () {
+            it('returns promise called with post(/history) with passed in valid params', function () {
+                var postSpy = sandbox.spy(request, 'post');
+                var sendSpy = sandbox.spy(request.Request.prototype, 'send');
+                var params = {
+                    currency: 'BTC',
+                    since: (new Date().getTime()),
+                    until: (new Date().getTime() + 10),
+                    limit: 1,
+                    wallet: 'deposit'
+                };
+
+                bitfinex.balanceHistory(params).should.have.property('then');
+                postSpy.should.have.been.calledWith('https://api.bitfinex.com/v1/history');
+                sendSpy.should.have.been.calledWith(params);
+            });
+        });
+
+        describe('movementsHistory function', function () {
+            it('returns promise called with post(/history/movements) with passed in valid params', function () {
+                var postSpy = sandbox.spy(request, 'post');
+                var sendSpy = sandbox.spy(request.Request.prototype, 'send');
+                var params = {
+                    currency: 'BTC',
+                    method: 'bitcoin',
+                    since: (new Date().getTime()),
+                    until: (new Date().getTime() + 10),
+                    limit: 1
+                };
+
+                bitfinex.movementsHistory(params).should.have.property('then');
+                postSpy.should.have.been.calledWith('https://api.bitfinex.com/v1/history/movements');
+                sendSpy.should.have.been.calledWith(params);
+            });
+        });
+
+        describe('pastTrades function', function () {
+            it('returns promise called with post(/mytrades) with passed in valid params', function () {
+                var postSpy = sandbox.spy(request, 'post');
+                var sendSpy = sandbox.spy(request.Request.prototype, 'send');
+                var params = {
+                    currency: 'BTC',
+                    timestamp: (new Date().getTime()),
+                    limit_trades: 1
+                };
+
+                bitfinex.pastTrades(params).should.have.property('then');
+                postSpy.should.have.been.calledWith('https://api.bitfinex.com/v1/mytrades');
+                sendSpy.should.have.been.calledWith(params);
+            });
+        });
+
+        describe('pastTrades function', function () {
+            it('returns promise called with post(/mytrades) with passed in valid params', function () {
+                var postSpy = sandbox.spy(request, 'post');
+                var sendSpy = sandbox.spy(request.Request.prototype, 'send');
+                var params = {
+                    currency: 'BTC',
+                    timestamp: (new Date().getTime()),
+                    limit_trades: 1
+                };
+
+                bitfinex.pastTrades(params).should.have.property('then');
+                postSpy.should.have.been.calledWith('https://api.bitfinex.com/v1/mytrades');
+                sendSpy.should.have.been.calledWith(params);
+            });
+        });
+
         describe('walletBalances function', function () {
             it('returns promise called with post(url) and set(headers)', function () {
                 var postSpy = sandbox.spy(request, 'post');
